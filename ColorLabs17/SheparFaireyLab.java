b12956ab9252d1ef4e7ae286c34db28a4b915fb1
Hashir Qureshi <hmqureshi@stu.naperville203.org>
2025-11-18 11:03:22 -0600

    finished labbs

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -20,31 +20,159 @@ public class SheparFaireyLab
         
          //opens selfie picture 
           /**/
-         String fileName = FileChooser.pickAFile();
-         Picture pictObj = new Picture(fileName);
-         pictObj.explore();
          
          //relative path
-         Picture apic = new Picture("images\\beach.jpg");
          //change with selfie picture
-         Picture me = new Picture("images/beach.jpg");
-         Picture me1 = new Picture("images/beach.jpg");
-         Picture me2 = new Picture("images/beach.jpg");
+         Picture me = new Picture("images/redMotorcycle.jpg");
+         Pixel[] pixels;
+         pixels = me.getPixels();
+         
+         Color bucket1 = new Color(255, 83, 64);
+         Color bucket2 = new Color(247, 244, 215);
+         Color bucket3 = new Color(114, 177, 204);
+         Color bucket4 = new Color(5, 19, 92);
          
          /**
           * method 1 change
           * 
+          * 
+          * 
           */
+         me = new Picture("images/redMotorcycle.jpg");
+         pixels = me.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                int avg = (int) ((r + g + b) / 3);
+                
+                if (avg < 64)
+                {
+                    pixels[i].setColor(bucket4);
+                }
+                else if (avg < 128)
+                {
+                    pixels[i].setColor(bucket3);
+                }
+                else if (avg < 192)
+                {
+                    pixels[i].setColor(bucket2);
+                }
+                else if (avg <= 255)
+                {
+                    pixels[i].setColor(bucket1);
+                }
+            }
+            me.explore();
+            me.write("images/zzMethod1.jpg");
          
          /**
           * method 2 change
           * 
           */
          
+         me = new Picture("images/redMotorcycle.jpg");
+         int max = 0;
+         int min = 255;
+         double avg = 0;
+         pixels = me.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                avg = (int) ((r + g + b) / 3);
+                
+                if (avg < min)
+                {
+                    min = (int) avg;
+                }
+                if (avg > max)
+                {
+                    max = (int) avg;
+                }
+            }
+            int range = max - min;
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                avg = (int) ((r + g + b) / 3);
+                if (avg < (int) (range / 4))
+                {
+                    pixels[i].setColor(bucket4);
+                }
+                else if (avg < (int) (range / 2))
+                {
+                    pixels[i].setColor(bucket3);
+                }
+                else if (avg < (int) ((range * 3)/ 4))
+                {
+                    pixels[i].setColor(bucket2);
+                }
+                else if (avg <= (int) (range))
+                {
+                    pixels[i].setColor(bucket1);
+                }
+            }
+            
+            
+            me.explore();
+            me.write("images/zzMethod2.jpg");
+         
+         
          /**
           * custom color palette
           */
-
+         
+          bucket1 = new Color(0, 0, 0); // black
+          bucket2 = new Color(0,255,64); // light green
+          bucket3 = new Color(190,92,255); //light purple
+          bucket4 = new Color(128,0,255); //purple
+          Color bucket5 = new Color(0,0,0); //red
+          
+          //blood red color scheme
+          //bucket1 = new Color(0, 0, 0); // black
+          //bucket2 = new Color(255, 0, 0); // bright red
+          //bucket3 = new Color(170, 0, 0); //red
+          //bucket4 = new Color(85, 0, 0); //dark red
+          //Color bucket5 = new Color(0, 0, 0); //black
+     
+         me = new Picture("images/me1.jpg");
+         pixels = me.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                 avg = (int) ((r + g + b) / 3);
+            
+                if (avg < 51)
+                {
+                    pixels[i].setColor(bucket5);
+                }
+                else if (avg < 102)
+                {
+                    pixels[i].setColor(bucket4);
+                }
+                else if (avg < 153)
+                {
+                    pixels[i].setColor(bucket3);
+                }
+                else if (avg < 204)
+                {
+                    pixels[i].setColor(bucket2);
+                }
+                else if (avg <= 255)
+                {
+                    pixels[i].setColor(bucket1);
+                }
+            }
+            
+            me.explore();
+            me.write("images/zzMethod3.jpg");
          
     }//main       
 }//class
--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -11,111 +11,155 @@ import java.util.*;
 import java.util.List; // resolves problem with java.awt.List and java.util.List
 public class TestPicture17
 {
-
-    /**
-     * main method, to test the picture
-     *
-     */
-  public static void main(String[] args)
-  {
-      //opens picture using a dialog box
-      /**/
-     String fileName = FileChooser.pickAFile();
-     Picture pictObj = new Picture(fileName);
-     pictObj.explore();
-
-     //opens a pictue using a path
-     //Picture apic = new Picture("C:\\Users\\khayes\\Favorites\\Documents\APCS- Java\chap03\Curriclum 2013\Picture Color labs\images\\beach.jpg");
-     
-     //relative path
-     Picture apic = new Picture("images\\beach.jpg");
-     Picture ferris1 = new Picture("images/2000 ferris wheel2.jpg");
-     Picture ferris2 = new Picture("images/2000 ferris wheel2.jpg");
-     Picture ferris3 = new Picture("images/2000 ferris wheel2.jpg");
-
-     //apic.explore();
-     ferris1.explore();
-     
-     //makes an array of pixels
-     Pixel[] pixels;
-     //gets pixels from picture and assigns to pixels array
-     pixels = ferris1.getPixels();
     
-     //how many pixels or how large array
-    System.out.println("This is a large array"+pixels.length  );
+        /**
+         * main method, to test the picture
+         *
+         */
+      public static void main(String[] args)
+      {
+          
+             
+            //relative path
+            Picture motorcycle = new Picture("images/redMotorcycle.jpg");
+            Pixel[] pixels;
 
-
-    /**/
-        //access each index
-    System.out.println(pixels[17]);
-    //access each pixel
-    Pixel spot = ferris1.getPixel(100,100);
-    
-    System.out.println(pixels[17].getColor());
-    System.out.println(spot);
-/*
-    pixels[17].setColor(Color.blue);
-    spot.setColor(new Color(252,252,252));
-    pixels[500034].setColor(Color.blue);
-
-    ferris1.explore();
-/*
-   // loop to access indexes of array or collection
-
-    //for each loop spot  is a ?
-    for (Pixel spot : pixels)
-    System.out.println( spot );
-
-
-   
- /**/
-
- /**
-  * Method to clear red from picture
-  * @param none
-  * @return none
-  */
- /*
-    for (Pixel pixelObj : pixels)
-        {
-            //set the red value of the current pixel to the new value
-           
-
-        }
-    ferris1.explore();
-    
-/**/
- /**
-  * Method to reduce red from picture by a factor of n
-  * @param none
-  * @return none
-  */
-
-/*
-int value;
-final double  FACTOR = .5;
-    for (Pixel pixelObj : pixels)
-    {
-
-        //get the redvalue
-        value = pixelObj.getRed();
-        //System.out.println(value);
-
-        //decrease the red value by 50%
-        
-        //set the red value of the current pixel to the new value
-        
-
-    }
-    // use new picture when changing or it will make changes to 
-    // pic you already changed
-    ferris1.explore();
-    ferris2.explore();
-
-  /**/ 
-    //write/save a picture as a file
-    ferris1.write("images/ferris11.jpg");
-
-    /**/
-  }//main
-}//class
+            
+            //Adjust blue by a factor
+            motorcycle = new Picture("images/redMotorcycle.jpg");
+            pixels = motorcycle.getPixels();
+            double factor = 2;
+            
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = (int) (pixels[i].getBlue() * factor);
+                
+                if (b > 255)
+                {
+                    b = 255;
+                }
+                
+                pixels[i].setColor(new Color(r, g, b));
+            }
+            motorcycle.explore();
+             
+            //Negate
+            motorcycle = new Picture("images/redMotorcycle.jpg");
+            pixels = motorcycle.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = Math.abs(255 - pixels[i].getRed());
+                int g = Math.abs(255 - pixels[i].getGreen());
+                int b = Math.abs(255 - pixels[i].getBlue());
+                pixels[i].setColor(new Color(r, g, b));
+            }
+            motorcycle.explore();
+            
+            
+            //Grayscale
+            pixels = motorcycle.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                int avg = (int) ((r + g + b) / 3);
+            
+                pixels[i].setColor(new Color(avg, avg, avg));
+            }
+            motorcycle.explore();
+            
+            
+            //Lighten
+            motorcycle = new Picture("images/redMotorcycle.jpg");
+            pixels = motorcycle.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = (int) (1.5 * pixels[i].getRed());
+                int g = (int) (1.5 * pixels[i].getGreen());
+                int b = (int) (1.5 * pixels[i].getBlue());
+                
+                if (r > 255)
+                {
+                    r = 255;
+                }
+                if (g > 255)
+                {
+                    g = 255;
+                }
+                if (b > 255)
+                {
+                    b = 255;
+                }
+                pixels[i].setColor(new Color(r, g, b));
+            }
+            motorcycle.explore();
+            
+            
+            //changecolor method
+            motorcycle = new Picture("images/redMotorcycle.jpg");
+            pixels = motorcycle.getPixels();
+            int adjRed = 50;
+            int adjGreen = 13;
+            int adjBlue = 90;
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed() + adjRed;
+                int g = pixels[i].getGreen() + adjGreen;
+                int b = pixels[i].getBlue() + adjBlue;
+                
+                if (r > 255)
+                {
+                    r = 255;
+                }
+                if (g > 255)
+                {
+                    g = 255;
+                }
+                if (b > 255)
+                {
+                    b = 255;
+                }
+                
+                if (r < 0)
+                {
+                    r = 0;
+                }
+                if (g < 0)
+                {
+                    g = 0;
+                }
+                if (b < 0)
+                {
+                    b = 0;
+                }
+                pixels[i].setColor(new Color(r, g, b));
+                
+            }
+            motorcycle.explore();
+            
+            
+            //Colorify method
+            motorcycle = new Picture("images/jenny-red.jpg");
+            pixels = motorcycle.getPixels();
+            for(int i = 0; i < pixels.length; i++)
+            {
+                int r = pixels[i].getRed();
+                int g = pixels[i].getGreen();
+                int b = pixels[i].getBlue();
+                
+                if (r > 100 && g < 60 && b < 60)
+                {
+                    g += 150;
+                    r -= 100;
+                }
+            
+                pixels[i].setColor(new Color(r, g, b));
+            }
+            motorcycle.explore();
+        }//main
+}
+  //class
+  
