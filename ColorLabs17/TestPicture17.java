11f58afffb551747b3080ec17fab4b576bcaf3ad
Hashir-Qureshi-ZZ <hmqureshi@stu.naperville203.org>
2026-05-04 11:24:35 -0500

    starting final project color labs

--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -19,14 +19,14 @@ public class TestPicture17
       public static void main(String[] args)
       {
           
-             
+             /*
             //relative path
-            Picture motorcycle = new Picture("images/redMotorcycle.jpg");
+            Picture motorcycle = new Picture("images/nicksKitchen.jpg");
             Pixel[] pixels;
 
             
             //Adjust blue by a factor
-            motorcycle = new Picture("images/redMotorcycle.jpg");
+            motorcycle = new Picture("images/nicksKitchen.jpg");
             pixels = motorcycle.getPixels();
             double factor = 2;
             
@@ -142,7 +142,7 @@ public class TestPicture17
             
             
             //Colorify method
-            motorcycle = new Picture("images/jenny-red.jpg");
+            motorcycle = new Picture("images/nicksKitchen.jpg");
             pixels = motorcycle.getPixels();
             for(int i = 0; i < pixels.length; i++)
             {
@@ -158,8 +158,51 @@ public class TestPicture17
             
                 pixels[i].setColor(new Color(r, g, b));
             }
-            motorcycle.explore();
-        }//main
-}
+            */
+            
+            
+            Picture nicksKitchen = new Picture("images/KSI.jpg");
+            //mirrorVertical(nicksKitchen);
+            nicksKitchen.explore();
+            }
+            
+    public static void mirrorVertical(Picture source)
+    {
+        int width = source.getWidth();
+        int mirrorPoint = width/2;
+        Pixel leftPixel = null;
+        Pixel rightPixel = null;
+        
+        for(int y = 0; y < source.getHeight(); y++)
+        {
+            // loop from 0 to the middle
+            for(int x = 0; x < mirrorPoint; x++)
+            {
+               leftPixel = source.getPixel(x, y);
+               rightPixel = source.getPixel(width - 1 - x, y);
+               rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+    
+    }
+    
+    public static void copytoCanvas(Picture source, Picture target)
+    {
+        Pixel sourcePix = null;
+        Pixel targetPix = null;
+        
+        for(int sourceX = 0, targetX = 0; sourceX < source.getWidth(); sourceX++, targetX++)
+        {
+            for(int sourceY = 0, targetY = 0; sourceY < source.getHeight(); sourceY++, targetY++)
+            {
+                sourcePix = source.getPixel(sourceX, sourceY);
+                targetPix = target.getPixel(targetX, targetY);
+                
+                targetPix.setColor(sourcePix.getColor());
+            }
+        }
+    }
+    }
+    //main
   //class
   
